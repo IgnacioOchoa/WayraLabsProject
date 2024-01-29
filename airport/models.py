524c23695f27c0fce46961dfe4a985401399172a
IgnacioOchoa <onacho87@gmail.com>
2024-01-29 00:14:20 -0300

    Agregados modelos de Node y Link
    Se incorporaron los modelos node y link con los campos mínimos
    indispensables. Se aplicaron las migraciones para agregarlos a la base
    de datos.

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+class Node(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
+
+class Link(models.Model):
+    node1 = models.ForeignKey(Node, on_delete=models.DO_NOTHING, related_name='link_node1')
+    node2 = models.ForeignKey(Node, on_delete=models.DO_NOTHING, related_name='link_node2')
+    description = models.TextField()
