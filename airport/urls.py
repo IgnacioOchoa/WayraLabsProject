1216c7487bb6d3969c37f1d917b3bb73b834b136
IgnacioOchoa <onacho87@gmail.com>
2024-01-28 23:36:17 -0300

    Adaptada estructura de app aeropuerto
    Se cambiaron algunos nombres de archivos, eliminaron propiedades de css
    innecesarias, y algunos botones que no se usaban.

--- airport/urls.py
+++ airport/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('airport/', views.geometryEdit, name="geometry_edit")
+        path('airport/', views.geometryEdit, name="airport_edit")
 ]
