c76a9c6b180e5ad2c9997e08a37ac63f4deec288
IgnacioOchoa <onacho87@gmail.com>
2024-01-30 22:47:24 -0300

    Nombres de Link y Node en tablas de admin

--- airport/models.py
+++ airport/models.py
@@ -5,7 +5,13 @@ class Node(models.Model):
     x = models.FloatField()
     y = models.FloatField()
 
+    def __str__(self):
+        return "Node " + str(self.id)
+
 class Link(models.Model):
     node1 = models.ForeignKey(Node, on_delete=models.DO_NOTHING, related_name='link_node1')
     node2 = models.ForeignKey(Node, on_delete=models.DO_NOTHING, related_name='link_node2')
     description = models.TextField()
+
+    def __str__(self):
+        return "Link {} (N{}-N{})".format(self.id,self.node1.id,self.node2.id)
