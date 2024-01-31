af834408e8a3b661705aa7aad9fcfff9b7c6d464
Gonzalo Vallejo <gvallejo.ing@gmail.com>
2024-01-31 19:51:56 -0300

    Changes in airport models

--- airport/models.py
+++ airport/models.py
@@ -2,16 +2,39 @@ from django.db import models
 # Create your models here.
 
 class Node(models.Model):
+    class Type(models.TextChoices):
+        INT = "1", "INTERSECTION"
+        THR = "2", "THRESHOLD/END"
+        DTH = "3", "DISPLACED THRESHOLD"
+        STP = "4", "STOPBAR"
     x = models.FloatField()
     y = models.FloatField()
+    z = models.FloatField()
+    type = models.CharField(
+        max_length=2,
+        choices=Type.choices,
+        default=Type.INT
+    )
+    runway_designator= models.CharField(
+        max_length=3,
+    )
 
     def __str__(self):
         return "Node " + str(self.id)
 
 class Link(models.Model):
+    class Type(models.TextChoices):
+        RWY = "1", "RUNWAY"
+        TWY = "2", "TAXIWAY"
+        APR = "3", "APRON"
     node1 = models.ForeignKey(Node, on_delete=models.DO_NOTHING, related_name='link_node1')
     node2 = models.ForeignKey(Node, on_delete=models.DO_NOTHING, related_name='link_node2')
     description = models.TextField()
+    type = models.CharField(
+        max_length=2,
+        choices=Type.choices,
+        default=Type.TWY
+    )
 
     def __str__(self):
-        return "Link {} (N{}-N{})".format(self.id,self.node1.id,self.node2.id)
+        return "Link {} (N{}-N{}) {}".format(self.id,self.node1.id,self.node2.id,self.get_type_display())
