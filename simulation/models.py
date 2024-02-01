2c8813b2623ded4495b760d647604731417ca60c
Gonzalo Vallejo <gvallejo.ing@gmail.com>
2024-02-01 18:51:49 -0300

    Simulation results updated

--- simulation/models.py
+++ simulation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from fleet.models import Aircraft
+from airport.models import Link, Node
 
 # Create your models here.
+class Results(models.Model):
+    fleet = models.ForeignKey(Aircraft, on_delete=models.CASCADE)
+    runway = models.ForeignKey(Link, on_delete=models.CASCADE)
+    threshold = models.ForeignKey(Node, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
--- simulation/views.py
+++ simulation/views.py
@@ -32,24 +32,36 @@ def simulation(request):
             runway_length = 3000
             total_operations = 0
             average_rot = 0
+            general_results = []
+
             for acft in fleet_aircrafts:
+                name = acft.aircraft.name
                 landing_speed = acft.aircraft.landing_speed
                 landing_distance = acft.aircraft.landing_distance
                 quantity = acft.quantity
                 acft_rot = rot(landing_speed, landing_distance, runway_length)
-                weighted_rot = acft_rot * quantity
-                average_rot += weighted_rot
+                results = {
+                    'acft': name,
+                    'landing_speed': landing_speed,
+                    'breaking_time': acft_rot[0],
+                    'taxiing_time': acft_rot[1],
+                    'rot': round(acft_rot[0] + acft_rot[1], 2),
+                    'quantity': quantity
+                }
+                general_results.append(results)
                 total_operations += quantity
+                average_rot += round(acft_rot[0] + acft_rot[1], 2)*quantity
             
             if total_operations == 0:
                 average_rot = 0
                 capacity = 0
             else:
-                average_rot = average_rot / total_operations
+                average_rot = round(average_rot / total_operations, 2)
                 capacity = int(3600 / average_rot)
 
             context['average_rot'] = average_rot
             context['arrivals_per_hour'] = capacity
+            context['general_results'] = general_results
 
             return render(request, 'simulation/simulation.html', context)
     else:
@@ -65,10 +77,9 @@ def rot(speed, distance, length):
     acceleration = -(speed**2)/(2*distance)
     breaking_distance = ((taxi_speed**2)-(speed**2))/(2*acceleration)
     leftover_distance = length - breaking_distance
-    breaking_time = (taxi_speed - speed)/acceleration
-    taxiing_time = leftover_distance / taxi_speed
-    rot = breaking_time + taxiing_time
-    return rot
+    breaking_time = round((taxi_speed - speed)/acceleration, 2)
+    taxiing_time = round(leftover_distance / taxi_speed, 2)
+    return breaking_time, taxiing_time
 
 def segment_length(n1, n2):
     segment_length = ((n1.x-n2.x)**2 + (n1.y-n2.y)**2)**0.5
